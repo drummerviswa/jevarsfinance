64706a66c9ca3abac69c1cea27cba86838984a0f
Viswanathan P <67451889+drummerviswa@users.noreply.github.com>
2024-06-24 19:56:58 +0530

    Completed

--- server/connect.js
+++ server/connect.js
@@ -7,21 +7,3 @@ export const db = mysql.createConnection({
     password:process.env.PASSWORD,
     database:process.env.DATABASE
 })
-function handleDisconnect() {
-    db.connect(function(err) {
-      if(err) { 
-        console.log('error when connecting to db:', err);
-        setTimeout(handleDisconnect, 2000); 
-      }
-    });
-    db.on('error', function(err) {
-      console.log('db error', err);
-      if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
-        handleDisconnect();
-      } else {
-        throw err;
-      }
-    });
-  }
-  
-  handleDisconnect();
