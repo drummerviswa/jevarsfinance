a7f6002ceab1679bc5b9e4403968f1705d13e4a8
Viswanathan P <67451889+drummerviswa@users.noreply.github.com>
2024-06-26 17:39:22 +0530

    Lands

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -30,6 +30,12 @@ export default function App() {
               Overall
             </Link>
             <Link
+              to="lands"
+              className="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"
+            >
+              Lands
+            </Link>
+            <Link
               to="register"
               type="button"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
